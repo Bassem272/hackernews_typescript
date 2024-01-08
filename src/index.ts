c667c6137936a9ba7d5cb859852a3ca09bb8acee
bassem <bassemgog@gmail.com>
2024-01-08 19:44:14 +0200

    update index.js and create scripts in package.json

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
 import { ApolloServer } from "apollo-server";
 import {schema }from "./schema";
 import { context } from "./context";
-// import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
 
 
 export const server = new ApolloServer({
     schema,
     context,
     introspection: true, // Ensure this is set to true
+    plugins:[ApolloServerPluginLandingPageLocalDefault()]
 });
 
-const port = 3000;
+const port = process.env.PORT||3000;
 
 server.listen({port}).then(({url}) => {
     console.log(`🚀  Server ready at ${url}`); 
